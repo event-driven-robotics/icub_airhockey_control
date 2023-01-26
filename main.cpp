0bcbea8a113d0c08624d7575e8ab905d141414a7
Massimiliano Iacono <massimiliano.iacono@iit.it>
2023-01-26 01:30:40 +0100

    Adding motion to first traj point

--- main.cpp
+++ main.cpp
@@ -40,15 +40,8 @@ int main(int argc, char const *argv[])
     {
         throw runtime_error("Something went wrong with model loading for kinematics/dynamics computation");
     }
-    BipedalLocomotion::IK::QPInverseKinematics QPIK;
 
-    Eigen::Vector3d base_position{0, 0, 0};
-    Eigen::VectorXd initial_joint_config(10);
-    initial_joint_config << 0, 0, 0, -29.4, 30.4, 0, 44.6, 0, 0, 0; // Initial joint configuration TODO
-    std::vector<double> right_arm_initial_joint_config {-29.4, 30.4, 0, 44.6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    const tuple<Eigen::Vector3d, manif::SO3d, Eigen::VectorXd> state{base_position, manif::SO3d::Identity(), initial_joint_config};
-    auto system = make_shared<CDS::FloatingBaseSystemKinematics>();
-    
+    // Open iCub devices for torso and right arm
     yarp::dev::ICartesianControl* cart_control;
 
     yarp::dev::IPositionControl* torso_ipos;
@@ -65,6 +58,13 @@ int main(int argc, char const *argv[])
     torso_driver.view(torso_iposd);
     torso_driver.view(torso_imod);
     torso_driver.view(torso_enc);
+    
+    int torso_naxes;
+    torso_ipos->getAxes(&torso_naxes);
+    std::vector<int> torso_modes(torso_naxes, VOCAB_CM_POSITION_DIRECT);
+    torso_imod->setControlModes(torso_modes.data()); 
+    std::vector<double>torso_encoder_values(torso_naxes);
+    while (!(torso_enc->getEncoders(torso_encoder_values.data()))) continue;
 
     yarp::dev::IPositionControl* right_arm_ipos;
     yarp::dev::IPositionDirect* right_arm_iposd;
@@ -81,23 +81,33 @@ int main(int argc, char const *argv[])
     right_arm_driver.view(right_arm_imod);
     right_arm_driver.view(right_arm_enc);
 
-    int torso_naxes;
-    torso_ipos->getAxes(&torso_naxes);
-    std::vector<int> torso_modes(torso_naxes, VOCAB_CM_POSITION);
-    torso_imod->setControlModes(torso_modes.data()); 
-
     int right_arm_naxes;
     right_arm_ipos->getAxes(&right_arm_naxes);
-
     std::vector<double>right_arm_encoder_values(right_arm_naxes);
-    right_arm_enc->getEncoders(right_arm_encoder_values.data());
-    std::vector<double>torso_encoder_values(torso_naxes);
-    torso_enc->getEncoders(torso_encoder_values.data());
 
-    std::vector<int> right_arm_modes(right_arm_naxes, VOCAB_CM_POSITION);
+    while (!(right_arm_enc->getEncoders(right_arm_encoder_values.data()))) continue;
+
+    std::vector<int> right_arm_modes(right_arm_naxes, VOCAB_CM_POSITION_DIRECT);
     right_arm_imod->setControlModes(right_arm_modes.data()); 
 
-    right_arm_ipos->positionMove(right_arm_initial_joint_config.data());
+    // Create IK related objects
+    BipedalLocomotion::IK::QPInverseKinematics QPIK;
+
+    // Base (robot root reference frame) is always static in this case
+    Eigen::Vector3d base_position{0, 0, 0};
+    manif::SO3d base_orientation = manif::SO3d::Identity();
+    Eigen::VectorXd initial_joint_config(10);
+    double integration_step = 0.01;
+
+    initial_joint_config << torso_encoder_values[0], torso_encoder_values[1], torso_encoder_values[2], // Torso roll, pitch, yaw
+                            right_arm_encoder_values[0], right_arm_encoder_values[1], right_arm_encoder_values[2], // Shoulder roll, pitch, yaw
+                            right_arm_encoder_values[3], // elbow
+                            right_arm_encoder_values[4], right_arm_encoder_values[5], right_arm_encoder_values[6]; // Wrist roll, pitch, yaw
+
+    cout << initial_joint_config << endl;
+
+    const tuple<Eigen::Vector3d, manif::SO3d, Eigen::VectorXd> state{base_position, base_orientation, initial_joint_config};
+    auto system = make_shared<CDS::FloatingBaseSystemKinematics>();
 
     kinDyn->setJointPos(initial_joint_config);
     system->setState(state);
@@ -105,7 +115,7 @@ int main(int argc, char const *argv[])
     CDS::ForwardEuler<CDS::FloatingBaseSystemKinematics> integrator;
 
     integrator.setDynamicalSystem(system);
-    integrator.setIntegrationStep(1);
+    integrator.setIntegrationStep(integration_step);
 
     auto params_handler = make_shared<BipedalLocomotion::ParametersHandler::YarpImplementation>();
     params_handler->setFromFile("../ik.ini");
@@ -126,32 +136,46 @@ int main(int argc, char const *argv[])
 
     QPIK.finalize(variables_handler);
 
-    std::fill(begin(right_arm_modes), end(right_arm_modes), VOCAB_CM_POSITION_DIRECT);
-    right_arm_imod->setControlModes(right_arm_modes.data());
+    std::vector<Eigen::Vector3d> trajectory_positions;
     
-    std::fill(begin(torso_modes), end(torso_modes), VOCAB_CM_POSITION_DIRECT);
-    torso_imod->setControlModes(torso_modes.data());
-
-
-
     for (int i = 0; i < 1000; ++i)
     {
-        Eigen::Vector3d T{-0.3, 0.2 * sin(M_2_PI * i / 1000), -0.1};
+        trajectory_positions.push_back(Eigen::Vector3d{-0.3, 0.2 * sin(M_2_PI * i / 1000), -0.1});
+    }
+
+    bool wp_reached = true;
+    auto traj_iter = trajectory_positions.begin();
+    
+    std::vector<int> joint_ids({0, 1, 2, 3, 4, 5, 6});
+
+    while (true)    
+    {
+        if (wp_reached){
+            wp_reached = false;
+            traj_iter ++;
+        }
+
+        auto T = *traj_iter;
         ee_task->setSetPoint(manif::SE3d(T, manif::SO3d::Identity()));
         QPIK.advance();
 
           
         system->setControlInput({{0, 0, 0, 0, 0, 0}, QPIK.getOutput().jointVelocity});
-        integrator.integrate(0, 0.01);
+        integrator.integrate(0, integration_step);
         const auto &[base_position, base_orientation, joint_position] = integrator.getSolution();
 
-        torso_iposd->setPositions(joint_position.head(2).data());
-        auto right_arm_pos = joint_position.tail(3);
-        std::vector<int> joint_ids({0, 1, 2, 3, 4, 5, 6});
+        torso_iposd->setPositions(joint_position.head(3).data());
+        auto right_arm_pos = initial_joint_config.tail(7);
+        cout << right_arm_pos;
         right_arm_iposd->setPositions(right_arm_pos.size(), joint_ids.data(), right_arm_pos.data());
+        while (!(right_arm_enc->getEncoders(right_arm_encoder_values.data()))) continue;
+        while (!(torso_enc->getEncoders(torso_encoder_values.data()))) continue;
 
+        joint_position({torso_encoder_values[0], torso_encoder_values[1], torso_encoder_values[2], // Torso roll, pitch, yaw
+                            right_arm_encoder_values[0], right_arm_encoder_values[1], right_arm_encoder_values[2], // Shoulder roll, pitch, yaw
+                            right_arm_encoder_values[3], // elbow
+                            right_arm_encoder_values[4], right_arm_encoder_values[5], right_arm_encoder_values[6]}); // Wrist roll, pitch, yaw
         kinDyn->setJointPos(joint_position);
-
 
     }
 
