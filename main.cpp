fe08ad5d86e9a49db2455b9c98f492b05a400475
Massimiliano Iacono <massimiliano.iacono@iit.it>
2023-02-02 15:39:12 +0000

    Fixed wrist orientation

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <BipedalLocomotion/ParametersHandler/YarpImplementation.h>
 #include <BipedalLocomotion/System/VariablesHandler.h>
 #include <Eigen/StdVector>
+#include <Eigen/Geometry>
 #include <cmath>
 #include <yarp/dev/IPositionDirect.h>
 #include <yarp/dev/IPositionControl.h>
@@ -17,6 +18,10 @@
 #include <yarp/dev/PolyDriver.h>
 #include <yarp/sig/Matrix.h>
 #include <yarp/math/Math.h>
+
+
+// -0.30 0.20 0.02 -0.14 -0.78 0.59 3.07
+
 
 using namespace std;
 namespace CDS = BipedalLocomotion::ContinuousDynamicalSystem;
@@ -28,7 +33,7 @@ int main(int argc, char const *argv[])
     iDynTree::ModelLoader modelLoader;
     vector<string> joint_names{"torso_yaw", "torso_roll", "torso_pitch", "r_shoulder_pitch", "r_shoulder_roll", "r_shoulder_yaw",
                           "r_elbow", "r_wrist_prosup", "r_wrist_pitch", "r_wrist_yaw"};
-    string device_name = "right_arm";
+
 
     if (!modelLoader.loadReducedModelFromFile("/usr/local/share/iCub/robots/iCubGenova02/model.urdf", joint_names))
     {
@@ -97,23 +102,12 @@ int main(int argc, char const *argv[])
     // Base (robot root reference frame) is always static in this case
     Eigen::Vector3d base_position{0, 0, 0};
     manif::SO3d base_orientation = manif::SO3d::Identity();
-    Eigen::VectorXd initial_joint_config(10);
     double integration_step = 0.001;
+    std::vector<int> right_arm_controlled_joint_ids({0,1,2,3,4,5,6});
+    Eigen::VectorXd joint_measured_pos(10);
+    Eigen::VectorXd joint_command_pos(10);
 
-    initial_joint_config << torso_encoder_values[0], torso_encoder_values[1], torso_encoder_values[2], // Torso roll, pitch, yaw
-                            right_arm_encoder_values[0], right_arm_encoder_values[1], right_arm_encoder_values[2], // Shoulder roll, pitch, yaw
-                            right_arm_encoder_values[3], // elbow
-                            right_arm_encoder_values[4], right_arm_encoder_values[5], right_arm_encoder_values[6]; // Wrist roll, pitch, yaw
-
-    initial_joint_config *= (M_PI / 180);
-
-    cout << initial_joint_config << endl;
-
-    const tuple<Eigen::Vector3d, manif::SO3d, Eigen::VectorXd> state{base_position, base_orientation, initial_joint_config};
     auto system = make_shared<CDS::FloatingBaseSystemKinematics>();
-
-    kinDyn->setJointPos(initial_joint_config);
-    system->setState(state);
 
     CDS::ForwardEuler<CDS::FloatingBaseSystemKinematics> integrator;
 
@@ -141,7 +135,7 @@ int main(int argc, char const *argv[])
     joint_regularization_task->initialize(params_handler->getGroup("JOINT_REGULARIZATION_TASK"));
 
     QPIK.addTask(ee_task, "ee_task", 0);
-    QPIK.addTask(joint_regularization_task, "joint_regularization_task", 1, Eigen::VectorXd::Ones(10));
+    QPIK.addTask(joint_regularization_task, "joint_regularization_task", 1, Eigen::VectorXd::Ones(10) * 0.1);
 
     QPIK.finalize(variables_handler);
 
@@ -155,8 +149,7 @@ int main(int argc, char const *argv[])
     bool wp_reached = true;
     auto traj_iter = trajectory_positions.begin();
     
-    std::vector<int> joint_ids({0, 1, 2, 3, 4, 5, 6});
-    Eigen::VectorXd joint_measured_pos(10);
+
 
     while (true)    
     {
@@ -164,10 +157,10 @@ int main(int argc, char const *argv[])
             wp_reached = false;
             traj_iter ++;
             Eigen::Vector3d T = *traj_iter;
-            manif::SO3d O = manif::SO3d(0, M_PI, 0);
+            Eigen::Vector4d q( 0, -0.7071068, 0.7071068, 0);
+            q.normalize();
+            manif::SO3d O = manif::SO3d(q);
             ee_task->setSetPoint(manif::SE3d(T, O));
-            cout << O.transform();
-
         }
 
 
@@ -178,7 +171,12 @@ int main(int argc, char const *argv[])
                             right_arm_encoder_values[3], // elbow
                             right_arm_encoder_values[4], right_arm_encoder_values[5], right_arm_encoder_values[6];
         joint_measured_pos *= (M_PI / 180);
+
+        joint_regularization_task->setSetPoint(joint_measured_pos, Eigen::VectorXd::Zero(joint_measured_pos.size()));
+        
+        const tuple<Eigen::Vector3d, manif::SO3d, Eigen::VectorXd> state{base_position, base_orientation, joint_measured_pos};
         kinDyn->setJointPos(joint_measured_pos);
+        system->setState(state);
 
         QPIK.advance();
         
@@ -186,16 +184,18 @@ int main(int argc, char const *argv[])
         system->setControlInput({{0, 0, 0, 0, 0, 0}, QPIK.getOutput().jointVelocity});
 
         integrator.integrate(0, integration_step);
-        const auto &[base_position, base_orientation, joint_position] = integrator.getSolution();
+        const auto &[base_position, base_orientation, joint_command_pos] = integrator.getSolution();
 
-        Eigen::VectorXd torso_command = joint_position.head(3);
+        Eigen::VectorXd torso_command = joint_command_pos.head(3);
         torso_command *= (180 / M_PI);
-        torso_iposd->setPositions(torso_command.data());
-        Eigen::VectorXd right_arm_pos = joint_position.tail(7);
-        right_arm_pos *= (180 / M_PI);
-        right_arm_iposd->setPositions(right_arm_pos.size(), joint_ids.data(), right_arm_pos.data());
+        // torso_iposd->setPositions(torso_command.data());
+        Eigen::VectorXd right_arm_command = joint_command_pos.tail(7);
+        right_arm_command *= (180 / M_PI);
+
+        cout << right_arm_command << "\n\n";
+        right_arm_iposd->setPositions(right_arm_command.size(), right_arm_controlled_joint_ids.data(), right_arm_command.data());
         
-        // cout << kinDyn->getWorldTransform("r_hand_dh_frame").asHomogeneousTransform().toString() << endl;        
+        cout << kinDyn->getWorldTransform("r_hand_dh_frame").asHomogeneousTransform().toString() << endl;
     }
 
     return 0;
